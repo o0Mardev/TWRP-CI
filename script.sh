5a2a7c6e4a22a7a031bd8d1fb20975423746d9ac
Hemanth <hemanthjabalpuri@gmail.com>
2023-06-11 22:52:47 +0530

    Oppo A91

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 MANIFEST_URL="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
-MANIFEST_BRANCH="twrp-12.1"
-DEVICE_TREE_URL="https://github.com/HemanthJabalpuri/twrp_motorola_rhode"
-DEVICE_TREE_BRANCH="touch"
-DEVICE_PATH="device/motorola/rhode"
+MANIFEST_BRANCH="twrp-11"
+DEVICE_TREE_URL="https://github.com/HemanthJabalpuri/twrp_OPPO_OP4B9B"
+DEVICE_TREE_BRANCH="test"
+DEVICE_PATH="device/OPPO/OP4B9B"
 COMMON_TREE_URL=""
 COMMON_PATH=""
-BUILD_TARGET="boot"
-TW_DEVICE_VERSION="5-test1"
+BUILD_TARGET="recovery"
+TW_DEVICE_VERSION="test1"
 
 DEVICE_NAME="$(echo $DEVICE_PATH | cut -d "/" -f 3)"
 case $MANIFEST_BRANCH in
@@ -37,9 +37,9 @@ sync() {
   repo sync -j$(nproc --all) --force-sync
 
   # Apply patches
-  cd system/core
-  curl -sL https://github.com/HemanthJabalpuri/twrp_motorola_rhode/files/11550608/dontLoadVendorModules.txt | patch -p 1
-  cd -
+  #cd system/core
+  #curl -sL https://github.com/HemanthJabalpuri/twrp_motorola_rhode/files/11550608/dontLoadVendorModules.txt | patch -p 1
+  #cd -
   
   # Clone device tree
   git clone $DEVICE_TREE_URL -b $DEVICE_TREE_BRANCH $DEVICE_PATH || abort "ERROR: Failed to Clone the Device Tree!"
